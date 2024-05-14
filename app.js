9d7457da431e630b20d196b0e995344d33c94f09
nicholas-stoll22 <nicholascourselstoll@gmail.com>
2024-05-14 15:00:35 -0500

    Initialized display value functionality and set it to only show 9 chars at once.

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("connected")
+let displayVal = '0';
+
+function display() {
+    display = document.getElementById('display');
+    display.innerText = displayVal;
+    if (displayVal.length > 9) {
+        display.innerText = displayVal.substring(0,9);
+    }
+}
+
+display();
